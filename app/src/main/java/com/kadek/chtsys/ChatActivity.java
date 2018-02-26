618a2aa9a404f9ac6eb14166795d6208dc1e9e1f
kadekchresna <kadekchresna@gmail.com>
2018-02-26 07:26:03 +0700

    finished on chat function and conversation but the image chat error

--- app/src/main/java/com/kadek/chtsys/ChatActivity.java
+++ app/src/main/java/com/kadek/chtsys/ChatActivity.java
@@ -1,9 +1,12 @@
 package com.kadek.chtsys;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Message;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +23,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -29,6 +34,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -63,6 +70,10 @@ public class ChatActivity extends AppCompatActivity {
 
     private static final int TOTAL_ITEMS_TO_LOAD = 10;
     private int mCurrentPage = 1;
+
+    private static final int GALLERY_PICK=1;
+
+    private StorageReference mImageStorage;
 
     private int itemPos = 0;
     private String mLastKey = "";
@@ -191,6 +202,17 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
+        mChatAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(galleryIntent, "SELECT IMAGE"), GALLERY_PICK);
+            }
+        });
+
 
         mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -206,6 +228,69 @@ public class ChatActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_PICK && resultCode == RESULT_OK){
+
+            Uri imageUri = data.getData();
+
+            final String current_user_ref = "messages/" + mCurrentUserId + "/" + mChatUser;
+            final String chat_user_ref = "messages/" + mChatUser + "/" + mCurrentUserId;
+
+            DatabaseReference user_message_push = mRootRef.child("messages")
+                    .child(mCurrentUserId).child(mChatUser).push();
+
+            final String push_id = user_message_push.getKey();
+
+
+            StorageReference filepath = mImageStorage.child("message_images").child( push_id + ".jpg");
+
+            filepath.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                    if(task.isSuccessful()){
+
+                        String download_url = task.getResult().getDownloadUrl().toString();
+
+
+                        Map messageMap = new HashMap();
+                        messageMap.put("message", download_url);
+                        messageMap.put("seen", false);
+                        messageMap.put("type", "image");
+                        messageMap.put("time", ServerValue.TIMESTAMP);
+                        messageMap.put("from", mCurrentUserId);
+
+                        Map messageUserMap = new HashMap();
+                        messageUserMap.put(current_user_ref + "/" + push_id, messageMap);
+                        messageUserMap.put(chat_user_ref + "/" + push_id, messageMap);
+
+                        mChatMessageView.setText("");
+
+                        mRootRef.updateChildren(messageUserMap, new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+
+                                if(databaseError != null){
+
+                                    Log.d("CHAT_LOG", databaseError.getMessage().toString());
+
+                                }
+
+                            }
+                        });
+
+
+                    }
+
+                }
+            });
+
+        }
     }
 
     private void loadMoreMessages() {
@@ -357,6 +442,14 @@ public class ChatActivity extends AppCompatActivity {
             messageUserMap.put(chat_user_ref + "/" + push_id, messageMap);
 
             mChatMessageView.setText("");
+
+
+
+            mRootRef.child("Chat").child(mCurrentUserId).child(mChatUser).child("seen").setValue(true);
+            mRootRef.child("Chat").child(mCurrentUserId).child(mChatUser).child("timestamp").setValue(ServerValue.TIMESTAMP);
+
+            mRootRef.child("Chat").child(mChatUser).child(mCurrentUserId).child("seen").setValue(false);
+            mRootRef.child("Chat").child(mChatUser).child(mCurrentUserId).child("timestamp").setValue(ServerValue.TIMESTAMP);
 
 
             mRootRef.updateChildren(messageUserMap, new DatabaseReference.CompletionListener() {
--- app/src/main/java/com/kadek/chtsys/MessageAdapter.java
+++ app/src/main/java/com/kadek/chtsys/MessageAdapter.java
@@ -6,11 +6,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -25,13 +30,13 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder>{
 
     private List<Messages> mMessageList;
-    private FirebaseAuth mAuth;
     private DatabaseReference mUserDatabase;
 
-    public MessageAdapter(List<Messages> mMessageList){
-        this.mMessageList = mMessageList;
-    }
+    public MessageAdapter(List<Messages> mMessageList) {
 
+        this.mMessageList = mMessageList;
+
+    }
 
     @Override
     public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -43,47 +48,68 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
 
     }
 
-    public class MessageViewHolder extends RecyclerView.ViewHolder{
-        public TextView messageText;
-        public TextView timeText;
-        public TextView displayName;
-        public CircleImageView profileImage;
+    public class MessageViewHolder extends RecyclerView.ViewHolder {
 
-        public MessageViewHolder(View view){
+        public TextView messageText;
+        public CircleImageView profileImage;
+        public TextView displayName;
+        public ImageView messageImage;
+
+        public MessageViewHolder(View view) {
             super(view);
 
-            messageText = (TextView)view.findViewById(R.id.message_text_layout);
-            profileImage = (CircleImageView)view.findViewById(R.id.message_profile_layout);
-            displayName = (TextView)view.findViewById(R.id.name_text_layout);
-            //timeText = (TextView)view.findViewById(R.id.message_layout)
+            messageText = (TextView) view.findViewById(R.id.message_text_layout);
+            profileImage = (CircleImageView) view.findViewById(R.id.message_profile_layout);
+            displayName = (TextView) view.findViewById(R.id.name_text_layout);
+            messageImage = (ImageView) view.findViewById(R.id.message_image_layout);
 
         }
     }
 
     @Override
-    public void onBindViewHolder(MessageViewHolder viewHolder, int i) {
-
-        mAuth = FirebaseAuth.getInstance();
-
-        String current_user_id = mAuth.getCurrentUser().getUid();
+    public void onBindViewHolder(final MessageViewHolder viewHolder, int i) {
 
         Messages c = mMessageList.get(i);
 
         String from_user = c.getFrom();
+        String message_type = c.getType();
 
-        if (from_user.equals(current_user_id)){
 
-            viewHolder.messageText.setBackgroundColor(Color.WHITE);
-            viewHolder.messageText.setTextColor(Color.BLACK);
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(from_user);
 
-        }else {
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-            viewHolder.messageText.setBackgroundResource(R.drawable.message_text_background);
-            viewHolder.messageText.setTextColor(Color.WHITE);
+                String name = dataSnapshot.child("name").getValue().toString();
+                String image = dataSnapshot.child("thumb_image").getValue().toString();
+
+                viewHolder.displayName.setText(name);
+
+                Picasso.with(viewHolder.profileImage.getContext()).load(image)
+                        .placeholder(R.drawable.avatar_pic).into(viewHolder.profileImage);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        if(message_type.equals("text")) {
+
+            viewHolder.messageText.setText(c.getMessage());
+            viewHolder.messageImage.setVisibility(View.INVISIBLE);
+
+
+        } else {
+
+            viewHolder.messageText.setVisibility(View.INVISIBLE);
+            Picasso.with(viewHolder.profileImage.getContext()).load(c.getMessage())
+                    .placeholder(R.drawable.avatar_pic).into(viewHolder.messageImage);
 
         }
-        viewHolder.messageText.setText(c.getMessage());
-        //viewHolder.timeText.setText(c.getMessage());
 
     }
 
@@ -91,4 +117,10 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     public int getItemCount() {
         return mMessageList.size();
     }
+
+
+
+
+
+
 }
